401aa5f8b43190f4d6ab927dd8252d2113f90be0
stamo15 <stamo15@alustudent.com>
2018-10-21 11:43:55 +0200

    Created function to get value and index of card clicked and updated the draw_cards() function to cater for different states

--- guess.py
+++ guess.py
@@ -11,6 +11,7 @@ WINDOW_HEIGHT = 500
 
 # You will probably still need quite a few globals
 cards = ['A', 'A', 'K', '2', '2', 'K']
+states = [1, 0, 1, 0, 2, 0]
 horiz_space_between_cards = (WINDOW_WIDTH - (len(cards) * CARD_WIDTH)) / (len(cards) + 1)
 vert_space_between_cards = (WINDOW_HEIGHT - 2) / 3
 
@@ -22,8 +23,16 @@ def draw_cards():
     card_y = vert_space_between_cards
 
     while count < len(cards):
-        img = load_image("assets/" + cards[count] + ".png")
-        draw_image(img, card_x, card_y)
+        if states[count] == 0:
+            set_fill_color(1, 0.5, 0)
+            draw_rectangle(card_x, card_y, CARD_WIDTH, CARD_HEIGHT)
+        elif states[count] == 1:
+            img = load_image("assets/" + cards[count] + ".png")
+            draw_image(img, card_x, card_y)
+        elif states[count] == 2:
+            set_fill_color(1, 1, 1)
+            set_stroke_color(1, 1, 1)
+            draw_rectangle(card_x, card_y, CARD_WIDTH, CARD_HEIGHT)
         card_x += CARD_WIDTH + horiz_space_between_cards
 
         count += 1
@@ -41,8 +50,8 @@ def check_mouse_input():
     if is_mouse_pressed():
         clicked_point_x = mouse_x()
         clicked_point_y = mouse_y()
-        # if is_mouse_on_card(clicked_point_x, clicked_point_y):
-        #     print(get_value_and_index(clicked_point_x, clicked_point_y))
+        if is_mouse_on_card(clicked_point_x, clicked_point_y):
+            print(get_value_and_index(clicked_point_x, clicked_point_y))
 
 
 def is_mouse_on_card(x, y):
@@ -60,7 +69,18 @@ def is_mouse_on_card(x, y):
     return False
 
 
-
+def get_value_and_index(x, y):
+    count = 1
+    value_and_index = []
+    while count < len(cards) + 1:
+        if horiz_space_between_cards * count + CARD_WIDTH * (count - 1) <= x <= (
+                horiz_space_between_cards + CARD_WIDTH) * count:
+            if vert_space_between_cards <= y <= vert_space_between_cards + CARD_HEIGHT or \
+                    WINDOW_HEIGHT - vert_space_between_cards - CARD_HEIGHT <= y <= WINDOW_HEIGHT - vert_space_between_cards:
+                value_and_index.append(cards[count - 1])
+                value_and_index.append(count - 1)
+                return value_and_index
+        count += 1
 
 def main():
     # As mentioned, this is just a suggested structure. Feel free to change this if you prefer
